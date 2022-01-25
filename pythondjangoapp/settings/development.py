5435750d5260ecdb91056954b3582ba9762f264c
Mohamed NourElDin <noureldin@eg.ibm.com>
2022-01-25 21:57:16 +0200

    Configure sqlite for development
    + turn on debug

--- pythondjangoapp/settings/development.py
+++ pythondjangoapp/settings/development.py
@@ -1,8 +1,16 @@
 from pythondjangoapp.settings.base import *
 
-DEBUG = False
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
 
 INSTALLED_APPS += (
-                   # other apps for local development
-                    'livereload',
-                   )
+    # other apps for local development
+    'livereload',
+)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(os.path.abspath(BASE_DIR), 'db.sqlite3'),
+    }
+}
