43cbadeb624dcdcef1d0fabebd5963df27d2c410
Mohamed NourElDin <noureldin@eg.ibm.com>
2022-01-26 01:38:16 +0200

    Add placeholder for untitled papers

--- medseer/models.py
+++ medseer/models.py
@@ -55,4 +55,4 @@ class Paper(models.Model):
     modified_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.title
+        return self.title or "<Untitled Paper>"
