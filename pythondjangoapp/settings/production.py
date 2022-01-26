ae2481fed82c3646fcf0168b65bfa6a40cb8cec3
Mohamed NourElDin <noureldin@eg.ibm.com>
2022-01-26 08:39:26 +0200

    Configure PostgreSQL database in production

--- pythondjangoapp/settings/production.py
+++ pythondjangoapp/settings/production.py
@@ -3,5 +3,16 @@ from pythondjangoapp.settings.base import *
 DEBUG = False
 
 INSTALLED_APPS += (
-                   # other apps for production
-                   )
+    # other apps for production
+)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['POSTGRES_DATABASE'],
+        'USER': os.environ['POSTGRES_USERNAME'],
+        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
+        'HOST': os.environ['POSTGRES_HOST'],
+        'PORT': os.environ['POSTGRES_PORT'],
+    }
+}
